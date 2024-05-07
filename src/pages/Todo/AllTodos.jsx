fd63e57ff9a097b54302b40c88e8f9e78cb0608e
Kshitish-the-developer-02 <kshitishnayak82@gmail.com>
2024-05-07 19:04:27 +0530

    make some changes

--- src/pages/Todo/AllTodos.jsx
+++ src/pages/Todo/AllTodos.jsx
@@ -9,40 +9,55 @@ import {
 } from "@material-tailwind/react";
 import { useAllTodosQuery } from "../../redux/api/todoApiSlice";
 import avatar from "../../assets/kshitish.jpg";
+import { MdDelete,MdEdit } from "react-icons/md";
+
 const AllTodos = () => {
   const { data: todos, isLoading } = useAllTodosQuery();
   return (
-    <div className=" flex flex-col justify-center items-center h-full gap-4">
+    <div className=" flex flex-col justify-center items-center h-full gap-0.5 p-1.5">
       {todos?.map((todo) => (
         <Card
           color="transparent"
           shadow={true}
-          className="w-full max-w-[26rem] border border-gray-300"
+          className="w-full xl:max-w-[40%] border border-gray-300 sm:max-w-[85%] mt-2 mb-2 shadow-lg"
         >
           <CardHeader
             color="transparent"
             floated={false}
             shadow={false}
-            className="mx-0 flex items-center gap-4 pt-0 pb-8"
+            className="mx-0 flex items-center gap-4 pt-0 p-4"
           >
-            <Avatar
-              size="lg"
-              variant="circular"
-              src={avatar}
-              alt="tania andrew"
-            />
+            <Avatar size="lg" variant="circular" src={avatar} alt="mine" />
             <div className="flex w-full flex-col gap-0.5">
               <div className="flex items-center">
                 <Typography variant="h5" color="blue-gray">
-                  {todo.title}
+                  <p className=" font-poppins text-xl font-bold text-[#22c55e] border border-gray-400 rounded-lg bg-[#dcfce7] border-transparent p-2">
+                    {todo.title}
+                  </p>
                 </Typography>
               </div>
             </div>
           </CardHeader>
           <CardBody className="mb-6 p-0">
-            <Typography>{todo.desc}</Typography>
             <Typography>
-              Due Date: {moment(todo.dueDate).format("Do MMMM YYYY")}
+              <p className=" font-poppins text-[16px] font-normal text-blue-600 p-4">
+                {todo.desc}
+              </p>
+            </Typography>
+            <Typography>
+              <div className=" flex justify-between">
+                <p className=" font-poppins p-4 text-[14px]">
+                  <span className=" font-extrabold text-black text-[16px] ">
+                    {" "}
+                    Due Date :{" "}
+                  </span>
+                  {moment(todo.dueDate).format("Do MMMM YYYY")}
+                </p>
+                <div className=" flex justify-center items-center gap-2 mr-4">
+                  <MdEdit size={24} color="blue"/>
+                  <MdDelete size={24} color="red"/>
+                </div>
+              </div>
             </Typography>
           </CardBody>
         </Card>
--- src/pages/Todo/TodoPage.jsx
+++ src/pages/Todo/TodoPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Breadcrumbs, Card, CardBody, Button } from "@material-tailwind/react";
+import { Card, CardBody, Button } from "@material-tailwind/react";
 import { Input, DatePicker } from "antd";
 import {
   useCreateTodoMutation,
@@ -88,128 +88,116 @@ const TodoPage = () => {
   };
   return (
     <>
-      <Breadcrumbs>
-        <a href="#" className="opacity-60">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-4 w-4"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-          </svg>
-        </a>
-        <a href="/" className="opacity-60">
-          <span>pages</span>
-        </a>
-        <a href="/todo">Todo-section</a>
-      </Breadcrumbs>
-      <div className="flex justify-center  h-auto w-auto">
+      <div className="flex justify-center  h-auto ">
         {/* <Card className="w-full "> */}
-          <CardBody >
-            <h1 className="font-poppins font-bold text-center text-2xl mb-4">
-              Todo List
-            </h1>
-            <form onSubmit={submitHandler}>
-              <div className=" text-left font-poppins">
-                <label htmlFor="todo title" className=" font-semibold">
-                  {" "}
-                  Title:
-                </label>
-                <Input
-                  type="text"
-                  placeholder="Enter your todo..."
-                  className="mb-4 font-poppins text-[14px]"
-                  value={todoInput}
-                  onChange={(e) => setTodoInput(e.target.value)}
-                  style={{ padding: "10px", marginTop: "10px" }}
-                />
-              </div>
-              <div className="text-left font-poppins">
-                <label htmlFor="desc" className=" font-semibold">
-                  Description:
-                </label>
-                <TextArea
-                  placeholder="Enter description of your todo here"
-                  rows={4}
-                  style={{ marginTop: "10px" }}
-                  value={todoDesc}
-                  onChange={(e) => setTodoDesc(e.target.value)}
-                  className="font-poppins text-[14px]"
-                />
-              </div>
-              <div className="text-left font-poppins mt-4 flex flex-col">
-                <label htmlFor="desc" className=" font-semibold">
-                  Due Date:
-                </label>
-                <DatePicker
-                  placeholder="Select date"
-                  style={{ marginTop: "10px", width: "50%" }}
-                  size="large"
-                  value={todoDate}
-                  onChange={(date) => setTodoDate(date)}
-                />
-              </div>
-              <div className="flex justify-end items-center">
-                <Button
-                  color={btnColor}
-                  buttonType="filled"
-                  size="regular"
-                  block={false}
-                  ripple="light"
-                  className=" mt-6"
-                  type="submit"
-                >
-                  {btnText}
-                </Button>
-              </div>
-            </form>
-
-            {recentTodos?.map((todo) => (
-              <div
-                key={todo.id}
-                className=" font-poppins font-semibold border border-gray-400 p-5  text-left rounded-md shadow-md mt-5 mb-4"
+        <CardBody>
+          <h1 className="font-poppins font-bold text-center text-2xl mb-4">
+            Todo List
+          </h1>
+          <form
+            onSubmit={submitHandler}
+            className=" border border-gray-400 p-4 rounded-xl shadow-xl"
+          >
+            <div className=" text-left font-poppins">
+              <label htmlFor="todo title" className=" font-semibold">
+                {" "}
+                Title:
+              </label>
+              <Input
+                type="text"
+                placeholder="Enter your todo..."
+                className="mb-4 font-poppins text-[14px] shadow-md"
+                value={todoInput}
+                onChange={(e) => setTodoInput(e.target.value)}
+                style={{ padding: "10px", marginTop: "10px" }}
+              />
+            </div>
+            <div className="text-left font-poppins">
+              <label htmlFor="desc" className=" font-semibold ">
+                Description:
+              </label>
+              <TextArea
+                placeholder="Enter description of your todo here"
+                rows={4}
+                style={{ marginTop: "10px" }}
+                value={todoDesc}
+                onChange={(e) => setTodoDesc(e.target.value)}
+                className="font-poppins text-[14px] shadow-md"
+              />
+            </div>
+            <div className="text-left font-poppins mt-4 flex flex-col">
+              <label htmlFor="desc" className=" font-semibold">
+                Due Date:
+              </label>
+              <DatePicker
+                placeholder="Select date"
+                style={{ marginTop: "10px", width: "50%" }}
+                size="large"
+                value={todoDate}
+                onChange={(date) => setTodoDate(date)}
+                className="shadow-md"
+              />
+            </div>
+            <div className="flex justify-start items-center">
+              <Button
+                color={btnColor}
+                buttonType="filled"
+                size="regular"
+                block={false}
+                ripple="light"
+                className=" mt-6 shadow-md"
+                type="submit"
               >
-                <ul>
-                  <li className=" flex flex-col">
-                    <h2 className=" font-bold text-[#22c55e] text-[22px] border border-gray-400 rounded-lg pt-2 pb-2 pl-2  bg-[#dcfce7] border-transparent">
-                      {todo.title}
-                    </h2>
-                    <p className=" mt-2 mb-2 pl-2 ">{todo.desc}</p>{" "}
-                    <div className=" mt-2 mb-0 flex  justify-between">
-                      <p className=" text-sm font-normal ">
-                        {" "}
-                        <span className=" font-extrabold text-black text-[14px] pl-2">
-                          Due Date:
-                        </span>{" "}
-                        {moment(todo.dueDate).format("Do MMMM YYYY")}
-                      </p>
-                      <div className=" flex flex-row gap-3">
-                        <MdEdit
-                          size={24}
-                          color="blue"
-                          onClick={() => handleEdit(todo)}
-                        />
-                        <MdDelete
-                          size={24}
-                          color="red"
-                          onClick={() => handleDelete(todo._id)}
-                        />
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            ))}
-            <Button
-              onClick={() => navigate("/show-all-todos")}
-              className=" text-[20px] mt-12 font-poppins "
-              fullWidth
-              style={{ textTransform: "none" }}
+                {btnText}
+              </Button>
+            </div>
+          </form>
+
+          {recentTodos?.map((todo) => (
+            <div
+              key={todo.id}
+              className=" font-poppins font-semibold border border-gray-400 p-5  text-left rounded-md shadow-md mt-5 mb-4"
             >
-              Show All Todo
-            </Button>
-          </CardBody>
+              <ul>
+                <li className=" flex flex-col">
+                  <h2 className=" font-bold text-[#22c55e] text-[22px] border border-gray-400 rounded-lg pt-2 pb-2 pl-2  bg-[#dcfce7] border-transparent">
+                    {todo.title}
+                  </h2>
+                  <p className=" mt-2 mb-2 pl-2 ">{todo.desc}</p>{" "}
+                  <div className=" mt-2 mb-0 flex  justify-between">
+                    <p className=" text-sm font-normal ">
+                      {" "}
+                      <span className=" font-extrabold text-black text-[14px] pl-2">
+                        Due Date:
+                      </span>{" "}
+                      {moment(todo.dueDate).format("Do MMMM YYYY")}
+                    </p>
+                    <div className=" flex flex-row gap-3">
+                      <MdEdit
+                        size={24}
+                        color="blue"
+                        onClick={() => handleEdit(todo)}
+                      />
+                      <MdDelete
+                        size={24}
+                        color="red"
+                        onClick={() => handleDelete(todo._id)}
+                      />
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          ))}
+          <Button
+            onClick={() => navigate("/show-all-todos")}
+            className=" text-[20px] mt-12 font-poppins "
+            fullWidth
+            style={{ textTransform: "none" }}
+          >
+            Show All Todo
+          </Button>
+        </CardBody>
         {/* </Card> */}
       </div>
     </>
