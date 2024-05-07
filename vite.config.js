e4c2c460511ef6e9accfda5beeef05f26198e19e
Kshitish-the-developer-02 <kshitishnayak82@gmail.com>
2024-05-07 17:12:19 +0530

    change backend live url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:8000",
+      "/api/": "https://portfolio-backend-z9pa.onrender.com",
     },
   },
 });
